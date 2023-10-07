0fe6aaed09fed9972bffe0a5b01a869ad886dd26
Faraon78 <olgaapt78@gmail.com>
2023-10-07 22:59:34 +0300

    refresh build folder

--- src/reduxTools/rootReducer.ts
+++ src/reduxTools/rootReducer.ts
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 
 import { burgerMenuReducer } from "./burgerMenu/burgerMenuReducer";
-import { rateReducer } from "./exchangeRate/rateReducer";
 import { formReducer } from "./formForOrderHouse/formReducer";
 import { apiRate } from "./requests/apiRate";
 import { api } from "./requests/apiRequests";
@@ -9,7 +8,6 @@ import { api } from "./requests/apiRequests";
 export const rootReducer = combineReducers({
   burgerMenu: burgerMenuReducer,
   form: formReducer,
-  rate:rateReducer,
   [api.reducerPath]: api.reducer,
   [apiRate.reducerPath]: apiRate.reducer,
 });
